50bdbcde3d9e1195334f44eb405723c55dd1ac03
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-07-18 16:49:59 +0500

    feat: initialize modern vite typescript environment for ui layer

--- backend/routes/hrRoutes.js
+++ backend/routes/hrRoutes.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 const hrController = require("../controllers/hrController");
-// Routes for HR 
 router.post("/apply", hrController.applyForJob);
 router.patch("/status/:id", hrController.updateCandidateStatus);
 router.get("/dashboard", hrController.getHRDashboard);
