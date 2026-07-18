fd386348a0d5a2b06b6278b1b134084149d0bd81
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2026-07-18 16:46:59 +0500

    feat: design mongoose schema for Job position details

--- backend/models/Job.js
+++ backend/models/Job.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const JobSchema = new mongoose.Schema({
   title: { type: String, required: true },
   department: String,
-  requirements: [String], // Array of skills needed
+  requirements: [String], // Array of skills neede
   description: String,
   postedDate: { type: Date, default: Date.now },
 });
